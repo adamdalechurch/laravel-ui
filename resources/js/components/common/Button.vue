2e42f5108d5f55f5b85d7ccfccf1c6bc7e139286
AdamChurch <38051837+adamdalechurch@users.noreply.github.com>
2022-10-31 11:15:42 -0400

    Button Component

---
+++ resources/js/components/common/Button.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <button @click="click" class='d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm'>
+        <slot></slot>
+    </button>
+</template>
+<script>
+export default {
+    name: "Button",
+    props: {
+    },
+    methods: {
+        click(){
+            this.$emit('click');
+        }
+    }
+}
+</script>
