c11f3bde1c6c99abe0bce63fbf34dd026a888c8c
AdamChurch <adamdchurch92@gmail.com>
2022-10-31 06:12:15 -0400

    My Groups - Basic AG Grid

--- resources/js/components/groups/MyGroups.vue
+++ resources/js/components/groups/MyGroups.vue
@@ -1,26 +1,64 @@
 <template>
     <div>
-        My Groups    
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">My Groups</h1>
+        </div>
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <x-button2 type='button' data-toggle="modal" data-target="#NewGroupModal"><i class="fas fa-plus fa-sm text-white-50"></i> New Group</x-button2>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive" v-if="rowData != null">
+                    <ag-grid-vue style="width: 100%; height: 500px;"
+                        class="ag-theme-alpine"
+                        :columnDefs="columnDefs"
+                        :rowData="rowData">
+                    </ag-grid-vue>
+                </div>
+                <div class="table-responsive" v-if="rowData == null">
+                    <i class="fas fa-circle-notch fa-spin"></i>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
+import { AgGridVue } from "ag-grid-vue";
+
 export default {
-  name: "MyGroups",
+  name: "My Groups",
   data() {
     return {
-        groups: []
-    }
+      columnDefs: null,
+      rowData: null,
+    };
   },
-//   methods: {
-//     CalcProgressPercent: CalcProgressPercent
-//   },
+  components: {
+    AgGridVue,
+  },
+  beforeMount() {
+    this.columnDefs = [
+      { field: "name" },
+      { field: "owner" },
+      { field: "roles" },
+      { field: "created_at" },
+      { field: "actions" }
+    ];
+  },
   async created(){
     axios
       .get(process.env.MIX_API_URL + '/api/groups')
       .then(response => {
-        this.groups = response.data;
-        console.log(this.groups);
+        this.rowData = response.data.groups;
       })
   }
-}
+};
 </script>
+<style lang="scss">
+   @import "~ag-grid-community/styles/ag-grid.css";
+   @import "~ag-grid-community/styles/ag-theme-alpine.css";
+</style>
+<style lang="scss">
+  @import "~ag-grid-community/styles/ag-grid.css";
+  @import "~ag-grid-community/styles/ag-theme-alpine.css";
+</style>
