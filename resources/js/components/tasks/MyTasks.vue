096bef1164b844ee00db9d0ce800d28360127a14
AdamChurch <38051837+adamdalechurch@users.noreply.github.com>
2022-11-04 07:29:59 -0400

    Page on My Tasks

--- resources/js/components/tasks/MyTasks.vue
+++ resources/js/components/tasks/MyTasks.vue
@@ -1,48 +1,52 @@
 <template>
-    <grid v-if="cols.length > 0"
-      :columnDefsProp="cols"
-      itemName='Task'
-      title='My Tasks'
-    >
-    <template v-slot="{item}">
-      <TaskForm :item="item" />
+  <page title="My Tasks">
+    <template v-slot:body>
+      <grid v-if="cols.length > 0"
+        :columnDefsProp="cols"
+        itemName='Task'
+        title='My Tasks'
+      >
+      <template v-slot="{item}">
+        <TaskForm :item="item" />
+      </template>
+      </grid>
     </template>
-    </grid>
-  </template>
-  <script>
-  import TaskForm from './TaskForm.vue';
-  import { TaskCols } from './TaskCols';
-  import StatusCell from "../common/StatusCell.vue";
+  </page>
+</template>
+<script>
+import TaskForm from './TaskForm.vue';
+import { TaskCols } from './TaskCols';
+import StatusCell from "../common/StatusCell.vue";
 
-  export default {
-    name: 'MyTasks',
-    data() {
-      return {
-        cols: TaskCols,
-        item: {},
-      }
-    },
-    components: {
-      TaskForm,
-      StatusCell
-    },
-    beforeMount() {
-      this.cols[2] = { 
-        headerName: "Status",
-        flex: 1,
-        cellRenderer: 'StatusCell',
-        cellRendererParams : {
-          updateStatus: this.updateStatus.bind(this)
-        }
-      }
-    },
-    methods:{
-      updateStatus: function(e, id){
-        axios
-          .post(`${process.env.MIX_API_URL}/api/tasks/${id}/update/status`, {status_id: e.target.value})
-          //.then(this.getItems());
+export default {
+  name: 'MyTasks',
+  data() {
+    return {
+      cols: TaskCols,
+      item: {},
+    }
+  },
+  components: {
+    TaskForm,
+    StatusCell
+  },
+  beforeMount() {
+    this.cols[2] = { 
+      headerName: "Status",
+      flex: 1,
+      cellRenderer: 'StatusCell',
+      cellRendererParams : {
+        updateStatus: this.updateStatus.bind(this)
       }
     }
+  },
+  methods:{
+    updateStatus: function(e, id){
+      axios
+        .post(`${process.env.MIX_API_URL}/api/tasks/${id}/update/status`, {status_id: e.target.value})
+        //.then(this.getItems());
+    }
   }
-  </script>
-   
+}
+</script>
+  
