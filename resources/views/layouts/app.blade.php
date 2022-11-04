3678c37b2dd342a86b8b0001e38b7304ea586b7a
AdamChurch <38051837+adamdalechurch@users.noreply.github.com>
2022-11-04 19:12:52 -0400

    Relative assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,14 +11,14 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Custom fonts for this template-->
-    <link href="{{env("APP_URL")}}/css/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="css/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="{{env("APP_URL")}}/css/sb-admin-2.min.css" rel="stylesheet">
-    <link href="{{env("APP_URL")}}/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="css/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 </head>
 <body id="page-top">
