5f8e9e4dbb9ffff79705a5c4c598063a4cee482d
AdamChurch <38051837+adamdalechurch@users.noreply.github.com>
2022-11-03 20:27:04 -0400

    Back nav link

--- resources/js/components/groups/Group.vue
+++ resources/js/components/groups/Group.vue
@@ -1,9 +1,15 @@
 <template>
   <page :title="group.name">
+    <template v-slot:backlink>
+      <router-link  :to="`/groups/`">
+        <i class='fas fa-w fa-arrow-left'></i> Back to Groups
+      </router-link>
+    </template>
     <template v-slot:header>
       <!-- content for the header slot -->
     </template>
     <template v-slot:body>
+      
       <fieldset disabled>
         <GroupForm :item="group"></GroupForm>
       </fieldset>
