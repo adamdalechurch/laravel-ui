3ccfff9049caa95d511a01c4b274f89ca67eb362
AdamChurch <38051837+adamdalechurch@users.noreply.github.com>
2022-11-06 18:48:43 -0500

    Updated Images.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
 
-
+</style>
 <div class="container">
 
 <div class="card o-hidden border-0 shadow-lg my-5">
     <div class="card-body p-0">
         <!-- Nested Row within Card Body -->
         <div class="row">
-            <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+            <div class="col-lg-5 d-none d-lg-block bg-register-image" style='background: url({{env("APP_URL")}}/img/register.jpg); background-size: cover; background-position-x: 35%'></div>
             <div class="col-lg-7">
                 <div class="p-5">
                     <div class="text-center">
