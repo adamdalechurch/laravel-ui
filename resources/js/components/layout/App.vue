4964b59e67e2511f4a88ea53cbbf1f47933fca25
AdamChurch <38051837+adamdalechurch@users.noreply.github.com>
2022-11-09 07:27:31 -0500

    Notifications view

--- resources/js/components/layout/App.vue
+++ resources/js/components/layout/App.vue
@@ -45,53 +45,13 @@
 
                     <!-- Nav Item - Alerts -->
                     <li class="nav-item dropdown no-arrow mx-1">
-                        <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
+                        <a class="nav-link dropdown-toggle" @click="toggleNotifications" href="#" id="alertsDropdown" role="button"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="fas fa-bell fa-fw"></i>
                             <!-- Counter - Alerts -->
                             <span v-if="user.new_notifications.length > 0" class="badge badge-danger badge-counter">{{user.new_notifications.length}}</span>
                         </a>
-                        <!-- Dropdown - Alerts -->
-                        <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                            aria-labelledby="alertsDropdown">
-                            <h6 class="dropdown-header">
-                                Alerts Center
-                            </h6>
-                            <a class="dropdown-item d-flex align-items-center" href="#">
-                                <div class="mr-3">
-                                    <div class="icon-circle bg-primary">
-                                        <i class="fas fa-file-alt text-white"></i>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="small text-gray-500">December 12, 2019</div>
-                                    <span class="font-weight-bold">A new monthly report is ready to download!</span>
-                                </div>
-                            </a>
-                            <a class="dropdown-item d-flex align-items-center" href="#">
-                                <div class="mr-3">
-                                    <div class="icon-circle bg-success">
-                                        <i class="fas fa-donate text-white"></i>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="small text-gray-500">December 7, 2019</div>
-                                    $290.29 has been deposited into your account!
-                                </div>
-                            </a>
-                            <a class="dropdown-item d-flex align-items-center" href="#">
-                                <div class="mr-3">
-                                    <div class="icon-circle bg-warning">
-                                        <i class="fas fa-exclamation-triangle text-white"></i>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="small text-gray-500">December 2, 2019</div>
-                                    Spending Alert: We've noticed unusually high spending for your account.
-                                </div>
-                            </a>
-                            <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
-                        </div>
+                        <Notifications  v-if="notificationsVisible" :notifications="user.new_notifications"></Notifications>
                     </li>
 
                     <div class="topbar-divider d-none d-sm-block"></div>
@@ -150,16 +110,20 @@
 
 <script>
 import { GetUsers } from '../profile/UserMethods';
-
+import Notifications from './Noitifications.vue';
     export default {
         Name: "App",
         data() {
             return {
                 menuOpen: true,
                 dropdownOpen: false,
+                notificationsVisible: false,
                 windowWidth: window.innerWidth,
                 user: {new_notifications: []}
             }
+        },
+        components:{
+            Notifications
         },
         mounted() {
             this.$nextTick(() => {
@@ -209,6 +173,9 @@ import { GetUsers } from '../profile/UserMethods';
                 .then(response => {
                     this.user = response.data.profile;
                 }) 
+            },
+            toggleNotifications(){
+                this.notificationsVisible = !this.notificationsVisible;
             }
         }
     }
---
+++ resources/js/components/layout/Noitifications.vue
@@ -0,0 +1,29 @@
+<template>
+<!-- Dropdown - Alerts -->
+<div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" style="display:block"
+                            aria-labelledby="alertsDropdown">
+        <h6 class="dropdown-header">
+            Notifications
+        </h6>
+        <a v-for="notification in notifications" :key="notification.id" class="dropdown-item d-flex align-items-center" href="#">
+            <div class="mr-3">
+                <div class="icon-circle bg-primary">
+                    <i class="fas fa-file-alt text-white"></i>
+                </div>
+            </div>
+            <div>
+                <div class="small text-gray-500">{{notification.subject}}</div>
+                <span class="font-weight-bold">{{notification.body}}</span>
+            </div>
+        </a>
+    </div>
+</template>
+
+<script>
+export default{
+    Name: "App",
+    props:{
+        notifications: []
+    }
+}
+</script>
